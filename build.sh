eb33743da9eb29ffaf391ca423479d3428b3c0f5
Wing Hon Lai <wlai@axway.com>
2023-01-19 23:54:43 +0000

    WIP: RDAPI-28682 enable Jenkins build and preview (#237)
    * RDAPI-28682 enable the use of Jenkins to build previews instead of Netlify.
    
    * RDAPI-28682 disable previews on the branch

--- build.sh
+++ build.sh
@@ -22,20 +22,18 @@
 #     the build script
 #   - all other files like content/en/ content will be picked up by hugo automatically
 #     if they change
-#   - 
 #
 
 set -e
 
 DEBUG=${DEBUG:-false}
 MODE=dev
-while getopts ":np" opt; do
+
+while getopts ":m:" opt; do
     case ${opt} in
-        n ) MODE=nelify
+        m ) MODE=$OPTARG
              ;;
-        p ) MODE=nelify-preview
-             ;;
-        * ) exit 1
+        * ) echo "[ERROR] Invalid option [${OPTARG}]!!";exit 1
             ;;
     esac
 done
@@ -64,7 +62,7 @@ function fCheckoutSubmodule() {
         exit 1
     fi
     # the npm packages doesn't seem to be needed on the netify build server...this is just for developers
-    if [[ "${MODE}" == "dev" ]];then
+    #if [[ "${MODE}" == "dev" ]];then
         echo "[INFO] Install npm packages required by docsy."
     	if [[ ! -d "node_modules" ]];then
             if [[ -f "package.json" ]];then
@@ -75,8 +73,9 @@ function fCheckoutSubmodule() {
     		    npm install -D --save postcss-cli
             fi
     	fi
-    fi
+    #fi
 }
+
 
 # fMergeContent:
 #  1. makes sure BUILD_DIR is clean
@@ -91,6 +90,7 @@ function fMergeContent() {
     local _c_context
     local _c_path
     local _c_name
+    local _branch_name
     local _ln_opt='-sf'
     if [[ "$DEBUG" == "true" ]];then
         _ln_opt='-vsf'
@@ -136,6 +136,17 @@ function fMergeContent() {
         fi
     done
 
+    # Update the github_branch Param value in config.toml. This is used by the github edit link. If
+    # the BRANCH_NAME is not set then it's either a local build or a PR. We don't want to enable the
+    # github edit link when in this scenario.
+    _branch_name=${BRANCH_NAME} # the BRANCH_NAME variable comes from Jenkins
+    if [[ ! "${_branch_name}" =~ ^"PR-"* ]];then
+        unlink build/config.toml
+        cp -f config.toml build/config.toml
+        sed -i "s|# github_branch|github_branch|g" build/config.toml
+        sed -i "s|github_branch = .*|github_branch = \"${_branch_name}\"|g" build/config.toml
+    fi
+
     # This soft link makes the git info available for hugo to populate the date with git hash in the footer.
     # Note that common files coming from axway-open-docs-common will not have this information and the pages
     # will use the "date" value at the top of the page.
@@ -153,19 +164,16 @@ function fRunHugo() {
     cd ${BUILD_DIR}
     mkdir public
     case "${MODE}" in
-        "dev") 
-            hugo server
-            ;;
-        "nelify") 
-            hugo
-            # Moving the "publish" directory to the ROOT of the workspace. Netlify can't publish a
-            # different directory even if the "Publish directory" is changed to specify a different directory.
-            mv -f ${BUILD_DIR}/public ${PROJECT_DIR}
-            ;;
-        "nelify-preview") 
-            hugo -b $DEPLOY_PRIME_URL
-            mv -f ${BUILD_DIR}/public ${PROJECT_DIR}
-            ;;
+      "dev") 
+          hugo server
+          ;;
+      "ci")
+          hugo
+          ;;
+      *)
+          echo "[ERROR] Build MODE [${MODE}] is invalid!!"
+          exit 1
+          ;;
     esac
 }
 
